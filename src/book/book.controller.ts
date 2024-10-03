dd22428d225e72492dd3de3b30f5540fae58442b
Adrian Vinícius Roma Alves <adrianromaalves@hotmail.com>
2024-10-03 17:44:43 -0300

    feat: add post and patch routes for book controller

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BooksMockData, IBooks } from './fakeDatabase';
+import { randomUUID } from 'crypto';
 
 @Controller('book')
 export class BookController {
@@ -11,8 +12,24 @@ export class BookController {
     return this.BookService.getAllBooks();
   }
 
-  @Get('getById/:id')
+  @Get(':id')
   getById(@Param('id') id: string): IBooks {
     return this.BookService.findBook(id);
+  }
+
+  @Post()
+  addBook(@Body() book: Partial<IBooks>): IBooks {
+    if (!book.author || !book.publicationYear || !book.title) {
+      return undefined;
+    }
+    return this.BookService.addBook(book);
+  }
+
+  @Put(':id')
+  updatedBook(
+    @Param('id') id: string,
+    @Body() book: Partial<IBooks>,
+  ): IBooks | undefined | string {
+    return this.BookService.updateBook(id, book);
   }
 }
