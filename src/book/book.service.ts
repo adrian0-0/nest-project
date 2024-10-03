783434a15ebad94053aeeb25bc0d009176a4920f
Adrian Vinícius Roma Alves <adrianromaalves@hotmail.com>
2024-10-03 17:45:10 -0300

    feat: add book and update book

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Param } from '@nestjs/common';
 import { BooksMockData, IBooks } from './fakeDatabase';
-import { uuid } from '../components/uuid';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class BookService {
@@ -10,5 +10,31 @@ export class BookService {
 
   findBook(bookId: string): IBooks | undefined {
     return BooksMockData.find((book) => book.id === bookId);
+  }
+
+  addBook(book: Partial<IBooks>): IBooks {
+    const newBookData: IBooks = {
+      id: randomUUID(),
+      author: book.author ?? '',
+      title: book.title ?? '',
+      publicationYear: book.publicationYear ?? 0,
+    };
+    BooksMockData.push(newBookData);
+    return newBookData;
+  }
+
+  updateBook(
+    bookId: string,
+    UpdatedBook: Partial<IBooks>,
+  ): IBooks | undefined | string {
+    const findBookToUpdate = BooksMockData.find((book) => book.id === bookId);
+    const updateBook = {
+      id: bookId ?? findBookToUpdate.id,
+      author: UpdatedBook.author ?? findBookToUpdate.author,
+      title: UpdatedBook.title ?? findBookToUpdate.title,
+      publicationYear:
+        UpdatedBook.publicationYear ?? findBookToUpdate.publicationYear,
+    };
+    return updateBook;
   }
 }
